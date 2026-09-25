79ba8483efb156c3d1fbc399853da3734a3d734e
Maxi <maximopadulagaido.mail@gmail.com>
2026-09-25 18:07:10 -0300

    feat: completar base de la clase Lobo

--- InterfazGraficaIE/src/Lobo.java
+++ InterfazGraficaIE/src/Lobo.java
@@ -7,6 +7,38 @@
  *
  * @author GZ TIENDA
  */
-public class Lobo {
+public class Lobo extends Animal implements Peligroso {
+
+    private int exitosCaza;  
+       
+    public Lobo(String nombre, double energia, int edad, boolean viva, int velocidad, double peso, int exitosCaza) {
+        
+        super(nombre, energia, edad, viva, velocidad, peso);
+    }
+    
+    public int getExitosCaza() {
+        return exitosCaza;
+    }
+
+    public void setExitosCaza(int exitosCaza) {
+        this.exitosCaza = exitosCaza;
+    }
+    
+    @Override
+    public void comer(Ecosistema eco) {
+    }
+
+    @Override
+    public void actuar(Ecosistema eco) {
+    }
+    
+    @Override
+    public void mostrarEstado() {
+    }
+    
+    @Override
+    public int getNivelPeligro() {
+        return 0;
+    }
     
 }
