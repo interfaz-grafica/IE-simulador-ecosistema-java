363c12698fa492cf93e6ec8a8d2f36d0dddf7348
Scarone Nahuel <nahuelscarone@gmail.com>
2026-09-24 21:39:57 -0300

    feat: crear base del proyecto

---
+++ InterfazGraficaIE/src/Entidad.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author GZ TIENDA
+ */
+public abstract class Entidad {
+
+    /*private String nombre;
+    private double energia;
+    private int edad;
+    private boolean viva;
+    
+    
+    public abstract actuar();*/
+}
