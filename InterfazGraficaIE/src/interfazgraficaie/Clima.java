de310e2ecaf7e611dbef867a442982686837b27f
Agutzy12 <agusbenitez12yhfgj@gmail.com>
2026-09-26 14:45:45 -0300

    feat: implementar enum Clima con factores meteorologicos y descripcion

--- InterfazGraficaIE/src/interfazgraficaie/Clima.java
+++ InterfazGraficaIE/src/interfazgraficaie/Clima.java
@@ -1,6 +1,23 @@
 package interfazgraficaie;
 
 public enum Clima {
-    
-    SOLEADO, LLUVIOSO, SEQUIA, INVIERNO;
+    SOLEADO("Soleado"),
+    LLUVIOSO("Lluvioso"),
+    SEQUIA("Sequía"),
+    INVIERNO("Invierno");
+
+    private final String descripcion;
+
+    Clima(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return descripcion;
+    }
 }
