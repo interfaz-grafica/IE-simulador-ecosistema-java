9a1282c0011c0554f53395cff47bbd61ee2c1914
Agutzy12 <agusbenitez12yhfgj@gmail.com>
2026-09-26 14:52:21 -0300

    feat: agregar factores de crecimiento y consumo al enum Clima

--- InterfazGraficaIE/src/interfazgraficaie/Clima.java
+++ InterfazGraficaIE/src/interfazgraficaie/Clima.java
@@ -1,19 +1,31 @@
 package interfazgraficaie;
 
 public enum Clima {
-    SOLEADO("Soleado"),
-    LLUVIOSO("Lluvioso"),
-    SEQUIA("Sequía"),
-    INVIERNO("Invierno");
+    SOLEADO("Soleado", 1.2, 1.0),
+    LLUVIOSO("Lluvioso", 1.5, 0.9),
+    SEQUIA("Sequía", 0.4, 1.3),
+    INVIERNO("Invierno", 0.2, 1.5);
 
     private final String descripcion;
+    private final double factorCrecimientoFlora;
+    private final double factorGastoEnergiaFauna;
 
-    Clima(String descripcion) {
+    Clima(String descripcion, double factorCrecimientoFlora, double factorGastoEnergiaFauna) {
         this.descripcion = descripcion;
+        this.factorCrecimientoFlora = factorCrecimientoFlora;
+        this.factorGastoEnergiaFauna = factorGastoEnergiaFauna;
     }
 
     public String getDescripcion() {
         return descripcion;
+    }
+
+    public double getFactorCrecimientoFlora() {
+        return factorCrecimientoFlora;
+    }
+
+    public double getFactorGastoEnergiaFauna() {
+        return factorGastoEnergiaFauna;
     }
 
     @Override
